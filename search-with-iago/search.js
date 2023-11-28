27f8cf502b6fb7e2ef8ce7350c5d3fc5c00d0648
Eon <eonward37@gmail.com>
2023-11-28 10:19:43 -0500

    initial

---
+++ search-with-iago/search.js
@@ -0,0 +1,133 @@
+// https://github.com/GoogleChrome/chrome-extensions-samples/blob/main/api-samples/contextMenus/basic/sample.js
+
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// A onclick callback function.
+chrome.contextMenus.onClicked.addListener(lookup);
+
+function lookup( data ) {
+    console.log( 'new query: ', data.selectionText );
+
+    const getWindows = async() => {
+        return chrome.windows.getAll();
+    }
+
+    (async () => {
+        console.log(await getWindows())
+    })();
+
+    chrome.tabs.create(
+        {
+            url: 'https://app.getiago.com/search/' + data.selectionText
+        }
+    )
+}
+
+chrome.runtime.onInstalled.addListener(function () {
+      // Create one test item for each context type.
+      let contexts = [ 'selection' ];
+
+      let context = contexts[0];
+      let title = "Iago search";
+      let id = context
+
+      chrome.contextMenus.create({
+        title: title,
+        contexts: [context],
+        id
+      });
+    });
+
+
+
+
+// // A generic onclick callback function.
+// function genericOnClick(info) {
+//   switch (info.menuItemId) {
+//     case 'radio':
+//       // Radio item function
+//       console.log('Radio item clicked. Status:', info.checked);
+//       break;
+//     case 'checkbox':
+//       // Checkbox item function
+//       console.log('Checkbox item clicked. Status:', info.checked);
+//       break;
+//     default:
+//       // Standard context menu item function
+//       console.log('Standard context menu item clicked.');
+//   }
+// }
+// chrome.runtime.onInstalled.addListener(function () {
+//   // Create one test item for each context type.
+//   let contexts = [
+//     'page',
+//     'selection',
+//     'link',
+//     'editable',
+//     'image',
+//     'video',
+//     'audio'
+//   ];
+//   for (let i = 0; i < contexts.length; i++) {
+//     let context = contexts[i];
+//     let title = "Test '" + context + "' menu item: ";
+//     chrome.contextMenus.create({
+//       title: title,
+//       contexts: [context],
+//       id: context
+//     });
+//   }
+
+//   // Create a parent item and two children.
+//   let parent = chrome.contextMenus.create({
+//     title: 'Test parent item',
+//     id: 'parent'
+//   });
+//   chrome.contextMenus.create({
+//     title: 'Child 1',
+//     parentId: parent,
+//     id: 'child1'
+//   });
+//   chrome.contextMenus.create({
+//     title: 'Child 2',
+//     parentId: parent,
+//     id: 'child2'
+//   });
+
+//   // Create a radio item.
+//   chrome.contextMenus.create({
+//     title: 'radio',
+//     type: 'radio',
+//     id: 'radio'
+//   });
+
+//   // Create a checkbox item.
+//   chrome.contextMenus.create({
+//     title: 'checkbox',
+//     type: 'checkbox',
+//     id: 'checkbox'
+//   });
+
+//   // Intentionally create an invalid item, to show off error checking in the
+//   // create callback.
+//   chrome.contextMenus.create(
+//     { title: 'Oops', parentId: 999, id: 'errorItem' },
+//     function () {
+//       if (chrome.runtime.lastError) {
+//         console.log('Got expected error: ' + chrome.runtime.lastError.message);
+//       }
+//     }
+//   );
+// })
---
+++ search-with-jisho/search.js
@@ -0,0 +1,43 @@
+// https://github.com/GoogleChrome/chrome-extensions-samples/blob/main/api-samples/contextMenus/basic/sample.js
+
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// A onclick callback function.
+chrome.contextMenus.onClicked.addListener(lookup);
+
+function lookup(data) {
+    console.log('new query: ', data.selectionText);
+
+    chrome.tabs.create(
+        {
+            url: 'https://jisho.org/search/' + data.selectionText
+        }
+    )
+}
+
+chrome.runtime.onInstalled.addListener(function () {
+    // Create one test item for each context type.
+    let contexts = ['selection'];
+
+    let context = contexts[0];
+    let title = "Jisho search";
+    let id = context
+
+    chrome.contextMenus.create({
+        title: title,
+        contexts: [context],
+        id
+    });
+});
